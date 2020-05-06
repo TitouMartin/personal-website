996af3595d329652145e202f6094c92fade63e53
titouan martin <titouan.martin@free.fr>
2020-05-06 13:31:43 +0200

    removed nuxt prebuild layout & style

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,5 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        personal-website
-      </h1>
-      <h2 class="subtitle">
-        portfolio
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <h1>Hi, I'm Titouan.</h1>
 </template>
 
 <script lang="ts">
@@ -35,35 +13,6 @@ export default Vue.extend({
 })
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="scss">
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
