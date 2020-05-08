9755d2feed7bd4706cffffe74c0f364c14e7c9f9
titouan martin <titouan.martin@free.fr>
2020-05-08 20:22:08 +0200

    small change on navbar links hover

--- layouts/default.vue
+++ layouts/default.vue
@@ -51,27 +51,47 @@
   }
 
   .link {
-  width: fit-content;
-  position: relative;
+    width: fit-content;
+    position: relative;
 
-  &:before {
-    content: '';
-    height: 3px;
-    width: 0px;
-    background-color: $text-color;
-    border-radius: 10px;
-    position: absolute;
-    top: 100%;
-    left: 50%;
-    transform: translateX(-50%);
-    @include transition;
-  }
+    &:before {
+      content: '';
+      height: 3px;
+      width: 0px;
+      background-color: $text-color;
+      border-radius: 10px;
+      position: absolute;
+      top: 100%;
+      left: 50%;
+      transform: translateX(-50%);
+      @include transition;
+    }
+
+    &:after {
+      content: '';
+      z-index: -1;
+      opacity: 0;
+      width: base-unit(2);
+      height: base-unit(2);
+      border-radius: 50%;
+      background: $button-background;
+      position: absolute;
+      top: calc(100% - #{base-unit(2)});
+      left: 0;
+      @include transition;
+    }
 
     &:hover {
       cursor: pointer;
 
       &:before {
         width: 100%;
+      }
+
+      &:after {
+        opacity: 1;
+        top: base-unit(0.5);
+        left: calc(100% - #{base-unit(2)});
       }
     }
   }
