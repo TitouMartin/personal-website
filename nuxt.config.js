d15f66a01278c401e83fa049b4a70c358e28f71d
titouan martin <titouan.martin@free.fr>
2020-07-26 17:53:10 +0200

    feat: added google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,9 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    { src: '~plugins/ga.js', mode: 'client' },
+  ],
   /*
    ** Nuxt.js dev-modules
    */
